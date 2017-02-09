79e3dc9993fc2884af8f050c8dcb9e7306c815db
Boris Yue <borisyue@yahoo.com>
2017-02-08 18:15:16 -0800

    Initial Commit

---
+++ Mini Project 1 (match members)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini Project 1 (match members)
+//
+//  Created by Boris Yue on 2/8/17.
+//  Copyright © 2017 Boris Yue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
